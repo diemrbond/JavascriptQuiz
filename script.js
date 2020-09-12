2017b556c64521c9966c60f38816b3c261fbb009
Andrew Kelleher <mrbondmustdie@gmail.com>
2020-09-12 23:33:00 +0800

    Countdown timer seutp, beginning load questions

--- script.js
+++ script.js
@@ -3,10 +3,11 @@
 var timeRemainingElement = document.getElementById("timeRemaining");
 
 // Start Screen
-var startScreen = document.getElementById("startScreen");
+var startScreenContainer = document.getElementById("startScreen");
 var startButton = document.getElementById("startQuiz");
 
 // Countdown to Start
+var countdownContainer = document.getElementById("countdown");
 var countdownStartElement = document.getElementById("countdownStart");
 
 // Variable Declaration
@@ -24,16 +25,37 @@ function init(){
 // Start Quiz
 function startQuiz(){
 
-    // Hide the start screen
-    startScreen.setAttribute("style", "display: none;");
+    // Hide the Start Screen
+    startScreenContainer.setAttribute("style", "display: none;");
+    
+    // Show the Countdown Timer
+    countdownContainer.setAttribute("style", "display: block;");
 
     // Start Countdown Timer
+    countdownStartElement.textContent = countDownToStart + "...";
     startQuizIn = setInterval(countdownStart, 1000);
+}
+
+// Load Questions
+function loadQuestions(){
+    
 }
 
 // Countdown Timers
 function countdownStart(){
-    
+    countDownToStart--;
+    if (countDownToStart <= 0){
+        // Stop the Countdown Timer
+        clearInterval(startQuizIn);
+        // Hide the Countdown Timer
+        countdownContainer.setAttribute("style", "display: none;");
+        // Load the Questions
+        loadQuestions();
+    }
+    else {
+        // Set the Countdown Timer to the current time
+        countdownStartElement.textContent = countDownToStart + "...";
+    }
 }
 
 // Start Program
